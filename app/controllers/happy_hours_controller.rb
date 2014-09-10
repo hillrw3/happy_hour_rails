643e548456133fc0270c8a61a2b81ef4e2e972ec
Rob Hill <robert.hill0122@gmail.com>
2014-09-10 11:54:54 -0600

    imported data from yelp into locations table. Map now renders points dynamically

--- app/controllers/happy_hours_controller.rb
+++ app/controllers/happy_hours_controller.rb
@@ -9,7 +9,28 @@ class HappyHoursController < ApplicationController
   end
 
   def create
-    
+    @happy_hour = Location.new(name: params[:location][:name],
+                               address: params[:location][:address],
+                               latitude: geocode_address(params[:location][:address])["lat"],
+                               longitude: geocode_address(params[:location][:address])["lng"],
+                               phone: params[:location][:phone]
+    )
+
+    if @happy_hour.save
+      flash[:notice] = "Location successfully added!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def geocode_address (address)
+    results = Geocoder.search(address).to_json
+    JSON.parse(results)[0]["data"]["geometry"]["location"]
   end
 
 end
